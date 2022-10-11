1341668d6d55b000060f2c354c53617cb2930daa
Yahya Ghoor <ghooryahya@gmail.com>
2022-10-11 13:34:42 +0200

    Create Label class with association to Item class

---
+++ lib/label.rb
@@ -0,0 +1,14 @@
+class Label
+  attr_reader :title, :color
+
+  def initialize(title, color)
+    @title = title
+    @color = color
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item
+    item.label = self
+  end
+end
