1d6a9be8a786151c00d148d69ada44b412b7d33d
Yahya Ghoor <ghooryahya@gmail.com>
2022-10-11 13:37:35 +0200

    Disable linter rule

--- lib/book.rb
+++ lib/book.rb
@@ -1,9 +1,11 @@
 class Book < Item
+  # rubocop:disable Metrics/ParameterLists
   def initialize(publisher, cover_state, publish_date, label: nil, source: nil, author: nil, genre: nil)
     super(publish_date, label: label, source: source, author: author, genre: genre)
     @publisher = publisher
     @cover_state = cover_state
   end
+  # rubocop:enable Metrics/ParameterLists
 
   private
 
